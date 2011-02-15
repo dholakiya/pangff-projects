5bd27ee31e311bdae59357ef070fe72a4c4be87c
pangff@gmail.com <pangff@gmail.com@659e0da7-44bf-0c00-ce7b-4610e5dd8811>
2011-02-15 11:30:56 +0000

    初始提交，fragment的一个非常简单的例子

---
+++ Api3.0/src/com/pangff/ArticleListFragment.java
@@ -0,0 +1,32 @@
+package com.pangff;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ArticleListFragment extends Fragment{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.listfragment, container, false);
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+	
+	
+
+}
